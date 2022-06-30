13ba0326aee3c25c37fca81f44df16dc8a1408cf
Sudhindra Pai K <sudhindra@Sudhindras-MacBook-Pro.local>
2022-06-30 18:14:50 +0530

    Worked on logout option

--- src/components/AppTopbar/AppTopBar.tsx
+++ src/components/AppTopbar/AppTopBar.tsx
@@ -1,9 +1,30 @@
 import {FC} from 'react';
-import {Wrapper} from './StyledAppTopBar';
+import {Wrapper, ProfielWapper, DropdownContainer, DropdownOption} from './StyledAppTopBar';
+
+import * as localStorageActionType from '../../LocalStorage/ActionTypes';
+import {setLocalStorage} from '../../LocalStorage/SetLocalStorage';
+
+import {adminRouts} from '../../routs';
+import {useNavigate} from 'react-router-dom';
 
 const AppTopBar:FC = () => {
+
+    const navigate = useNavigate();
+
+    const clearLocalStorage = () => {
+        setLocalStorage(localStorageActionType.CLEAR_LOGIN_USER_DETAIL,"");
+        navigate(adminRouts.login);
+    };
+
     return (<Wrapper>
-        Profile
+        <ProfielWapper>
+            Profile
+        <DropdownContainer>
+            <DropdownOption onClick={clearLocalStorage} >
+                Logout
+            </DropdownOption>
+        </DropdownContainer>
+        </ProfielWapper>
     </Wrapper>)
 };
 
--- src/components/AppTopbar/StyledAppTopBar.ts
+++ src/components/AppTopbar/StyledAppTopBar.ts
@@ -13,3 +13,40 @@ padding: 0 40px;
 box-shadow: 0px 2px 0px rgba(0, 0, 0, 0.04);
 border-bottom: 1px solid #F6F6F6;
 `;
+
+export const DropdownContainer = styled.div`
+position: absolute;
+top: auto;
+right: -30px;
+width: 140px;
+z-index: 1;
+box-shadow: 0px 4px 44px rgb(0 0 0 / 12%);
+  background-color: #FFFFFF;
+  border-radius: 5px;
+  display: none;
+`;
+
+
+export const ProfielWapper = styled.div`
+position: relative;
+margin-right: 20px;
+&:hover{
+    ${DropdownContainer}{
+        display: block;
+    }
+}
+`;
+
+
+export const DropdownOption = styled.div`
+width: 100%;
+box-sizing: border-box;
+padding: 12px 16px;
+border-bottom: 1px solid #f7f7f7;
+color: #000000;
+text-align: left;
+cursor: pointer;
+&:last-child{
+    border-bottom: none;
+}
+`;
